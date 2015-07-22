d7b63d9460f510881e195b4f8dd5c963dcd46ab4
Chris Loew <cloew123@gmail.com>
2015-07-22 15:45:24 -0600

    Added basic C++ wrapper code

---
+++ cpp/src/NytramBox2D.cpp
@@ -0,0 +1,6 @@
+#include "NytramBox2D.h"
+
+unsigned int Add_World()
+{
+	return 0;
+}
---
+++ cpp/src/NytramBox2D.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define NYTRAM_API __declspec(dllexport)
+
+#if __cplusplus
+extern "C" {
+#endif
+
+	unsigned int Add_World();
+	
+#if __cplusplus
+}
+#endif
