5139e3048a7d934da51f2f2f89f236e56d656245
Chris Loew <cloew123@gmail.com>
2015-07-22 15:09:32 -0600

    Added Boilerplate Package code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='nytram_box2d',
+      version='0.0.1',
+      description="",
+      author='',
+      author_email='',
+      packages=['nytram_box2d'],
+     )
