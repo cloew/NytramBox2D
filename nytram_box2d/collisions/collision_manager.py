655683e9587d5c9772ba414ccb49d30199b53b8a
Chris Loew <cloew123@gmail.com>
2015-07-27 16:04:46 -0600

    Now use the kao sequence library rather than a custom Incrementer class

--- nytram_box2d/collisions/collision_manager.py
+++ nytram_box2d/collisions/collision_manager.py
@@ -1,10 +1,4 @@
-
-def Incrementer(startAt=0):
-    """ Return the next value by incrementing from the start at value """
-    value = startAt
-    while True:
-        yield value
-        value += 1
+from kao_sequence import Incrementer
         
 class CollisionManager:
     """ Helper class to manage connections between colliders and collisions """
