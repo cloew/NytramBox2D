66005f149543f27e200ddc9448e21fabb4093db4
Chris Loew <cloew123@gmail.com>
2015-07-27 12:08:38 -0600

    Prevented fixture defs from being copied..r?

--- cpp/src/NytramBox2D.cpp
+++ cpp/src/NytramBox2D.cpp
@@ -26,14 +26,14 @@ const b2Vec2& Body_GetPosition(unsigned int bodyId)
 	return bodyManager.getPosition(bodyId);
 }
 
-unsigned int Body_AddBoxFixture(unsigned int bodyId, const FixtureDef fixtureDef, float width, float height)
+unsigned int Body_AddBoxFixture(unsigned int bodyId, const FixtureDef& fixtureDef, float width, float height)
 {
 	b2PolygonShape box;
 	box.SetAsBox(width/2, height/2);
 	return fixtureManager.addFixture(bodyManager.getBody(bodyId), &ToFixtureDef(fixtureDef, &box));
 }
 
-unsigned int Body_AddEdgeFixture(unsigned int bodyId, const FixtureDef fixtureDef, const b2Vec2& v1, const b2Vec2& v2)
+unsigned int Body_AddEdgeFixture(unsigned int bodyId, const FixtureDef& fixtureDef, const b2Vec2& v1, const b2Vec2& v2)
 {
 	b2EdgeShape edge;
 	edge.Set(v1, v2);
--- cpp/src/NytramBox2D.h
+++ cpp/src/NytramBox2D.h
@@ -22,8 +22,8 @@ extern "C" {
 
 	// Body
 	NYTRAM_API const b2Vec2& Body_GetPosition(unsigned int bodyId);
-	NYTRAM_API unsigned int Body_AddBoxFixture(unsigned int bodyId, const FixtureDef fixtureDef, float width, float height);
-	NYTRAM_API unsigned int Body_AddEdgeFixture(unsigned int bodyId, const FixtureDef fixtureDef, const b2Vec2& v1, const b2Vec2& v2);
+	NYTRAM_API unsigned int Body_AddBoxFixture(unsigned int bodyId, const FixtureDef& fixtureDef, float width, float height);
+	NYTRAM_API unsigned int Body_AddEdgeFixture(unsigned int bodyId, const FixtureDef& fixtureDef, const b2Vec2& v1, const b2Vec2& v2);
 
 	// Collision
 	NYTRAM_API void Collision_SetStartCallback(Collision_Callback_fp callback);
