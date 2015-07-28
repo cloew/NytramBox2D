7cf6a5c9e540e2e3e8760242fbb4d7a5c3a2eba8
Chris Loew <cloew123@gmail.com>
2015-07-28 10:44:31 -0600

    The world now steps based on the milliseconds since the last frame

--- nytram_box2d/world.py
+++ nytram_box2d/world.py
@@ -4,10 +4,12 @@ from ctypes import byref
 class World:
     """ Represents a World in the Box2D Engine """
     
-    def __init__(self, gravity=Vec2(0, 0)):
+    def __init__(self, gravity=Vec2(0, 0), velocityIterations=10, positionIterations=10):
         """ Initialize the world """
         self.id = CppEngine.World_Add(byref(gravity))
+        self.velocityIterations = velocityIterations
+        self.positionIterations = positionIterations
         
-    def step(self, timeStep, velocityIterations=10, positionIterations=10):
+    def step(self, msSinceLastFrame):
         """ Step the world """
-        CppEngine.World_Step(self.id, timeStep, velocityIterations, positionIterations)
+        CppEngine.World_Step(self.id, msSinceLastFrame/1000, self.velocityIterations, self.positionIterations)
