c9add8c80a2a2b5cb8c3a6fd78e5ae6b73d3b84b
Chris Loew <cloew123@gmail.com>
2015-07-28 11:38:45 -0600

    Updated the body so that it acts as a behavior

--- nytram_box2d/body.py
+++ nytram_box2d/body.py
@@ -4,11 +4,25 @@ from .engine.body_def import BodyDef
 class Body:
     """ Represents a Box2D Body """
     
-    def __init__(self, world, **kwargs):
+    def __init__(self, fixtures, **kwargs):
         """ Initialize the Body """
-        self.id = Box2DEngine.World_AddBody(world.id, BodyDef(**kwargs))
+        self.id = None
+        self.fixtures = fixtures
+        self.bodyDef = BodyDef(**kwargs)
         
     @property
     def position(self):
         """ Return the position """
         return Box2DEngine.Body_GetPosition(self.id).contents
+        
+    def start(self):
+        """ Start the body in the Physics Engine """
+        self.id = Box2DEngine.World_AddBody(self.world.id, self.bodyDef)
+        
+        for fixture in self.fixtures:
+            fixture.addToBody(self)
+        
+    @property
+    def world(self):
+        """ Return the world this body is associated with """
+        return self.entity.scene.world
--- nytram_box2d/fixture.py
+++ nytram_box2d/fixture.py
@@ -3,6 +3,12 @@ from .engine.fixture_def import FixtureDef
 class Fixture:
     """ Represents a fixture to apply to a body """
     
-    def __init__(self, body, shape, **kwargs):
+    def __init__(self, shape, **kwargs):
         """ Initialize the fixture """
-        self.id = shape.apply(body, FixtureDef(**kwargs))
+        self.id = None
+        self.fixtureDef = FixtureDef(**kwargs)
+        self.shape = shape
+        
+    def addToBody(self, body):
+        """ Add the fixture to the given body """
+        self.id = self.shape.apply(body, self.fixtureDef)
